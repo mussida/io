3e5bb79c57c4cc4271801ce1dc5f79594f71da64
mussida <96059227+mussida@users.noreply.github.com>
2025-01-03 10:35:43 +0100

    feat: add msr collection

--- src/app/pages/msr-collection/msr-collection.component.ts
+++ src/app/pages/msr-collection/msr-collection.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ThumbnailComponent } from '../../components/thumbnail/thumbnail.component';
+import { NgxSplideComponent, NgxSplideModule } from 'ngx-splide';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-msr-collection',
   standalone: true,
-  imports: [ThumbnailComponent],
+  imports: [NgxSplideModule, CommonModule],
   templateUrl: './msr-collection.component.html',
   styleUrl: './msr-collection.component.css',
 })
-export class MsrCollectionComponent {}
+export class MsrCollectionComponent {
+  @ViewChild('mainSplide') gridThumbnailSplide: NgxSplideComponent | undefined;
+  images = [
+    'https://myportfolioarchive.s3.eu-north-1.amazonaws.com/90150007.JPG',
+    'https://myportfolioarchive.s3.eu-north-1.amazonaws.com/redcar.JPG',
+    'https://myportfolioarchive.s3.eu-north-1.amazonaws.com/90160005.JPG',
+  ];
+}
